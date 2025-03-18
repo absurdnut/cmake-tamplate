b7bc4113a5823e85cc17b9818c789eabff8a943a
absurdnut <guopyran@outlook.com>
2025-03-18 19:20:36 +0800

    rm the build and .cache

--- build/CMakeFiles/MySuperProject.dir/compiler_depend.make
+++
@@ -1,2 +0,0 @@
-# Empty compiler generated dependencies file for MySuperProject.
-# This may be replaced when dependencies are built.
--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/guopyran/development/demo/cmake-test
-# Build directory: /home/guopyran/development/demo/cmake-test/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
